74b97bce29f6147916acf27de8f017a8f900b7a9
Eduardo Escares <eduardoscrs60@gmail.com>
2023-09-03 15:43:22 -0300

    css

--- js/Funciones.js
+++ js/Funciones.js
@@ -30,7 +30,7 @@ document.getElementById('formulario-contacto').addEventListener('submit', functi
   event.preventDefault(); // Prevenir el comportamiento predeterminado del formulario
   mostrarSeccion('contacto'); // Mostrar la sección de "Contacto"
   document.getElementById('mensaje-enviado').style.display = 'block'; // Mostrar el mensaje de éxito
-
+  document.getElementById('formulario-contacto').reset()
   // Restablecer el formulario después de mostrar el mensaje (opcional)
-  document.getElementById('formulario-contacto').reset();
+  ;
 });
