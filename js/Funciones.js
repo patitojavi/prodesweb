1ee020e20cb87fc44356fb4107ed4f4907e6f051
Eduardo Escares <eduardoscrs60@gmail.com>
2023-09-03 14:00:02 -0300

    contacto

--- js/Funciones.js
+++ js/Funciones.js
@@ -5,7 +5,6 @@ const enlacesNavegacion = document.querySelectorAll('.menu a');
 const secciones = document.querySelectorAll('.seccion');
 
 // Función para mostrar una sección y ocultar las demás
-
 function mostrarSeccion(seccion) {
     var secciones = document.getElementsByClassName('seccion');
     for (var i = 0; i < secciones.length; i++) {
@@ -14,13 +13,21 @@ function mostrarSeccion(seccion) {
   
     if (seccion === 'contacto') {
       document.getElementById('contacto').style.display = 'block';
+      document.getElementById('mensaje-enviado').style.display = 'none'; // Ocultar el mensaje de éxito al mostrar "Contacto"
     } else {
       document.getElementById(seccion).style.display = 'block';
+      document.getElementById('mensaje-enviado').style.display = 'none'; // Ocultar el mensaje de éxito al mostrar otras secciones
     }
-  }
+}
 
 // Mostrar la sección "Home" por defecto al cargar la página
 window.addEventListener('load', () => {
     mostrarSeccion('home');
 });
 
+// Evento de escucha para el formulario de contacto
+document.getElementById('formulario-contacto').addEventListener('submit', function (event) {
+    event.preventDefault(); // Prevenir el comportamiento predeterminado del formulario
+    mostrarSeccion('contacto'); // Mostrar la sección de "Contacto"
+    document.getElementById('mensaje-enviado').style.display = 'block'; // Mostrar el mensaje de éxito
+});
