cdd82fb3227183980d44dca1eb21851742fefa6d
Eduardo Escares <eduardoscrs60@gmail.com>
2023-10-17 18:50:10 -0300

    .

--- js/mostrar-seccion.js
+++ js/mostrar-seccion.js
@@ -1,13 +1,13 @@
-function mostrarSeccion(seccionId) {
-    // Obtener todas las secciones con la clase "seccion" y ocultarlas
-    var secciones = document.querySelectorAll('.seccion');
-    for (var i = 0; i < secciones.length; i++) {
-        secciones[i].style.display = 'none';
-    }
+// function mostrarSeccion(seccionId) {
+//     // Obtener todas las secciones con la clase "seccion" y ocultarlas
+//     var secciones = document.querySelectorAll('.seccion');
+//     for (var i = 0; i < secciones.length; i++) {
+//         secciones[i].style.display = 'none';
+//     }
     
-    // Mostrar la sección deseada
-    var seccion = document.getElementById(seccionId);
-    if (seccion) {
-        seccion.style.display = 'block';
-    }
-}
+//     // Mostrar la sección deseada
+//     var seccion = document.getElementById(seccionId);
+//     if (seccion) {
+//         seccion.style.display = 'block';
+//     }
+// }
