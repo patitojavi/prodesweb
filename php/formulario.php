dd33969f18488e14b2ff7425addda5bb9e7a3c78
Patricio Benavides <patojavier1981@gmail.com>
2023-10-17 16:49:18 -0300

    actualizacion php

---
+++ php/formulario.php
@@ -0,0 +1,21 @@
+<?php
+include('conex.inc.php');
+
+if ($conn->connect_error) {
+    die("Error de conexión a la base de datos: " . $conn->connect_error);
+}
+
+$nombre = $_POST["nombre"];
+$email = $_POST["email"];
+$mensaje = $_POST["mensaje"];
+
+$insert_query = "INSERT INTO contactos (nombre, email, mensaje) VALUES ('$nombre', '$email', '$mensaje')";
+
+if ($conn->query($insert_query) === TRUE) {
+    echo "<script>alert('Los datos se han guardado correctamente.'); window.location = '/html/contacto.html';</script>";
+} else {
+    echo "Error: " . $insert_query . "<br>" . $conn->error;
+}
+
+$conn->close();
+?>
