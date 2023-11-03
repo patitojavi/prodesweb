d7176929127ee1f59a962e768d0aa62ce5010a14
Eduardo Escares <eduardoscrs60@gmail.com>
2023-11-03 02:50:03 -0300

    a

---
+++ js/mantencion-pc.js
@@ -0,0 +1,14 @@
+var xhr = new XMLHttpRequest();
+xhr.open('GET', 'servicio.php?categoria=pc', true);
+
+xhr.onload = function () {
+    if (xhr.status === 200) {
+        var data = JSON.parse(xhr.responseText);
+        // Procesa y muestra los datos en el espacio "content" en tu página
+        // Puedes recorrer el array "data" para mostrar la información en la página.
+    } else {
+        console.error('Error al obtener datos');
+    }
+};
+
+xhr.send();
