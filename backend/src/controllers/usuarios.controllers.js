e57a6f0530cf826f6f6834a7c82d3f011c579d3d
Eduardo Escares <eduardoscrs60@gmail.com>
2023-11-25 17:54:27 -0300

    a

--- backend/src/controllers/usuarios.controllers.js
+++ backend/src/controllers/usuarios.controllers.js
@@ -1,89 +1,68 @@
 import { pool } from '../config/bd.js'
 
 
-
+// Obtener todos los usuarios
 export const getUsuarios = async (req, res) => {
-    
     try {
-        const [rows] = await pool.query('SELECT * FROM Usuario')
-        res.json(rows)
+        const [rows] = await pool.query('SELECT ID, Nombre, Apellido, Correo, Contraseña, rol FROM Usuario');
+        res.json(rows);
     } catch (error) {
         console.error('Error al obtener los usuarios:', error);
         res.status(500).json({ message: "Error al obtener los usuarios" });
-    }    
+    }
 }
 
-
-//const sql = 'SELECT * FROM usuarios'
-
-
+// Obtener un usuario por su ID
 export const getUsuario = async (req, res) => {
-
     try {
-        const [rows] = await pool.query('SELECT * FROM Usuario WHERE ID = ?', [req.params.id])
-        if (rows.length <= 0) return res.status(404).json({ message: "El usuario no existe" })
-        res.json(rows[0])
-    } catch (error) {       
+        const [rows] = await pool.query('SELECT ID, Nombre, Apellido, Correo, Contraseña, rol FROM Usuario WHERE ID = ?', [req.params.id]);
+        if (rows.length <= 0) return res.status(404).json({ message: "El usuario no existe" });
+        res.json(rows[0]);
+    } catch (error) {
         console.error('Error al obtener el usuario:', error);
         res.status(500).json({ message: "Error al obtener el usuario" });
-        
     }
-
 }
 
+// Crear un nuevo usuario
 export const createUsuarios = async (req, res) => {
     try {
-        const { ID, Nombre, Apellido, Correo, Contraseña } = req.body;
-        // Verificar si el usuario ya existe en la base de datos
+        const { ID, Nombre, Apellido, Correo, Contraseña, rol } = req.body;
         const [existingUsers] = await pool.query('SELECT * FROM Usuario WHERE Correo = ?', [Correo]);
         if (existingUsers.length > 0) {
             return res.status(400).json({ message: "El usuario ya existe" });
         }
-        // Si el usuario no existe, insertarlo en la base de datos
-        const [rows] = await pool.query('INSERT INTO Usuario (ID, Nombre, Apellido, Correo, Contraseña) VALUES (?,?,?,?,?)', [ID, Nombre, Apellido, Correo, Contraseña]);
-        console.log(req.body);
-        res.send({
-            ID: rows.insertId, Nombre, Apellido, Correo, Contraseña
-        });
+        const [rows] = await pool.query('INSERT INTO Usuario (ID, Nombre, Apellido, Correo, Contraseña, rol) VALUES (?,?,?,?,?,?)', [ID, Nombre, Apellido, Correo, Contraseña, rol]);
+        res.send({ ID: rows.insertId, Nombre, Apellido, Correo, Contraseña, rol });
     } catch (error) {
         console.error('Error al crear el usuario:', error);
         res.status(500).json({ message: "Error al crear el usuario" });
     }
 }
 
-
-//const sql = 'INSERT INTO usuarios SET ?'
-
-
+// Eliminar un usuario por su ID
 export const deleteUsuarios = async (req, res) => {
     try {
-        const [rows] = await pool.query('DELETE FROM Usuario WHERE ID = ?', [req.params.id])
-        if (rows.affectedRows <= 0) return res.status(404).json({ message: "El usuario no existe" })
-        res.json({ message: "El usuario ha sido eliminado" })
+        const [rows] = await pool.query('DELETE FROM Usuario WHERE ID = ?', [req.params.id]);
+        if (rows.affectedRows <= 0) return res.status(404).json({ message: "El usuario no existe" });
+        res.json({ message: "El usuario ha sido eliminado" });
     } catch (error) {
         console.error('Error al eliminar el usuario:', error);
         res.status(500).json({ message: "Error al eliminar el usuario" });
-        
-    }     
-
+    }
 }
-//const sql = 'DELETE FROM usuarios WHERE id = ?'
 
+// Actualizar un usuario por su ID
 export const updateUsuarios = async (req, res) => {
     try {
         const { id } = req.params;
-        const { Nombre, Apellido, Correo, Contraseña } = req.body;
-
-        // Verificar si el usuario existe en la base de datos
+        const { Nombre, Apellido, Correo, Contraseña, rol } = req.body;
         const [existingUsers] = await pool.query('SELECT * FROM Usuario WHERE ID = ?', [id]);
         if (existingUsers.length === 0) {
             return res.status(404).json({ message: "El usuario no existe" });
         }
-
-        // Realizar la actualización
-        const [results] = await pool.query('UPDATE Usuario SET Nombre = IFNULL(?, Nombre), Apellido = IFNULL(?, Apellido), Correo = IFNULL(?, Correo), Contraseña = IFNULL(?, Contraseña) WHERE ID = ?', [Nombre, Apellido, Correo, Contraseña, id]);
-
-        const [rows] = await pool.query('SELECT * FROM Usuario WHERE ID = ?', [id]);
+        const [results] = await pool.query('UPDATE Usuario SET Nombre = IFNULL(?, Nombre), Apellido = IFNULL(?, Apellido), Correo = IFNULL(?, Correo), Contraseña = IFNULL(?, Contraseña), rol = IFNULL(?, rol) WHERE ID = ?', [Nombre, Apellido, Correo, Contraseña, rol, id]);
+        const [rows] = await pool.query('SELECT ID, Nombre, Apellido, Correo, Contraseña, rol FROM Usuario WHERE ID = ?', [id]);
         res.json(rows[0]);
     } catch (error) {
         console.error('Error al actualizar el usuario:', error);
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,33 +1,32 @@
-import express from "express"
-import cors from "cors"
-import userRoutes from "./routes/usuarios.routes.js"
-import rutasproducto from "./routes/productos.routes.js"
-import cuentaRoutes from "./routes/cuenta.routes.js"
-import { PORT } from './config/config.js'
+import express from "express";
+import cors from "cors";
+import userRoutes from "./routes/usuarios.routes.js";
+import rutasproducto from "./routes/productos.routes.js";
+import cuentaRoutes from "./routes/cuenta.routes.js";
+import { PORT } from './config/config.js';
 
-const PORT = process.env.PORT || 3000;
+const app = express(); // Crear la instancia de la aplicación Express
 
-// cors: permite que se puedan realizar peticiones desde un dominio diferente
-// al del servidor como puede ser el caso de un cliente web.
+// cors y express.json deben configurarse antes de definir las rutas
 app.use(cors({
 	origin: 'http://127.0.0.1:5500',
 	methods: 'GET,POST,PUT',
 	credentials: true
-}))
+}));
 
-// express.json() recibe y convierte la informacion que viene especificada en el header
-// como tipo json a un objeto de js para que pueda ser utilizado por el servidor
-app.use(express.json())
+app.use(express.json());
 
-// aqui se declaran las rutas o end-points del servidor 
-app.use('/api', userRoutes)
-app.use('/api', rutasproducto)
-app.use('/api', cuentaRoutes)
+// Definir las rutas
+app.use('/api', userRoutes);
+app.use('/api', rutasproducto);
+app.use('/api', cuentaRoutes);
 
-// ruta/end-point para las rutas que no se han definido en el servidor
+// Endpoint para rutas no definidas
 app.use((req, res) => {
-	res.status(404).json({ message: "Endpoint not found" })
-})
+	res.status(404).json({ message: "Endpoint not found" });
+});
 
-// El servidor se pone a escuchar en el puerto PORT
-app.listen(PORT)
+// Escuchar en el puerto PORT
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
