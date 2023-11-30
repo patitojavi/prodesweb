de37f72e1a5490f83394a52bda6b29f57d2e5ab0
Eduardo Escares <eduardoscrs60@gmail.com>
2023-11-30 01:31:21 -0300

    aaaaaaaaaaaaa

--- backend/src/config/config.js
+++ backend/src/config/config.js
@@ -1,11 +1,22 @@
-import 'dotenv/config'
+import 'dotenv/config';
 
-export const PORT = process.env.PORT || 3000
-export const JWT_SECRET = process.env.JWT_SECRET || "perfumeria_secret"
-export const DB_HOST = process.env.MODE === "PRODUCCION" ? process.env.DB_HOST : "localhost"
-export const DB_USER = process.env.MODE === "PRODUCCION" ? process.env.DB_USER : "root"
-export const DB_PASSWORD = process.env.MODE === "PRODUCCION" ? process.env.DB_PASSWORD : ""
-export const DB_URL = process.env.MODE === "PRODUCCION" ? process.env.DB_URL : "localhost"
-export const DB_PORT = process.env.MODE === "PRODUCCION" ? process.env.DB_PORT : 3306
-export const DB_DATABASE = process.env.MODE === "PRODUCCION" ? process.env.DB_DATABASE : "joyeria"
-export const DB_CONNECT_TIMEOUT = 100000
+const express = require('express');
+const mysql = require('mysql2');
+
+const app = express();
+const PORT = 3000;
+
+const db = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE
+});
+
+// Conectar a la base de datos
+db.connect((err) => {
+  if (err) {
+    throw err;
+  }
+  console.log('Conexión a la base de datos exitosa');
+});
